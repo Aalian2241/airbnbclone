277f065167e28829ad4ddefe3a69dc8055d45152
Aalian2241 <aalian.2241@gmail.com>
2023-05-08 12:09:30 +0500

    navbar made responsive

--- src/Components/FilterNavbar/FilterNavbar.js
+++ src/Components/FilterNavbar/FilterNavbar.js
@@ -7,16 +7,17 @@ const FilterNavbar = () => {
   const [cols, setCols] = useState(7);
 
   const [activeButton, setActiveButton] = useState(false);
+  const [toggleFilter,setToggleFilter] = useState(false);
   useEffect(() => {
     function handleResize() {
       if (window.innerWidth<320){
-        setCols(1);
+        setCols(3);
       }
       else if (window.innerWidth<500){
-        setCols(2);
+        setCols(4);
       }
         else if (window.innerWidth < 700) {
-        setCols(3);
+        setCols(5);
       }
         else if (window.innerWidth < 1024) {
         setCols(6);
@@ -24,7 +25,6 @@ const FilterNavbar = () => {
         setCols(7);
       }
     }
-
     window.addEventListener('resize', handleResize);
     handleResize();
 
@@ -32,17 +32,34 @@ const FilterNavbar = () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+
+  useEffect(()=>{
+    function handleFilterToggle(){
+      window.innerWidth <768?setToggleFilter(false):setToggleFilter(true);
+
+    }
+    window.addEventListener('resize', handleFilterToggle);
+    handleFilterToggle();
+
+    return () => {
+      window.removeEventListener('resize', handleFilterToggle);
+    };
+
+  },[])
   function handleClick(index) {
     setActiveButton(index);
   }
   
   return (
-        <Carousel  
+    
+    <div className="relative ">
+      <div className="md:mr-[6.6rem]">
+      <Carousel  
             cols={cols}
             // containerStyle={{flexGrow:1}} 
             rows={1} 
-
-            loop={false}
+            containerStyle={{}}
+            loop={true}
             mobileBreakpoint={330}
             scrollSnap={true}
 
@@ -50,10 +67,10 @@ const FilterNavbar = () => {
             >
             
             {filter_items.map((item,index) => (
-              <Carousel.Item >
+              <Carousel.Item className="flex justify-center">
                 <button 
                 onClick={() => handleClick(index)}
-                className={`flex items-center border-b flex-col py-[0.2rem] md:py-[0.75rem] ${activeButton === index ? "visited" : ""}`} >
+                className={`flex items-center mx-auto hover:border-b-gray-500 border-b flex-col py-[0.6rem] md:py-[0.75rem]  ${activeButton === index ? "visited" : ""}`} >
                   <img src={item.src} className="h-[1.3rem]"/>
                   <span className='text-[0.75rem] text-center'>
                     {item.desc}
@@ -63,9 +80,16 @@ const FilterNavbar = () => {
             ))}
 
         </Carousel>
-        
-
-        
-  )
+      </div>
+      {
+        toggleFilter &&
+        <button className='absolute right-0 top-[0.7rem] flex justify-center items-center mr-[0.6rem] rounded-lg p-2 shadow-lg border-slate-300 border '>
+        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" className='block h-4 w-4 mr-[0.6rem]'><path d="M5 8c1.306 0 2.418.835 2.83 2H14v2H7.829A3.001 3.001 0 1 1 5 8zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm6-8a3 3 0 1 1-2.829 4H2V4h6.17A3.001 3.001 0 0 1 11 2zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path></svg>
+        <span>Filters</span>
+      </button>
+      }
+      
+    </div>
+      )
 }
 export default FilterNavbar
